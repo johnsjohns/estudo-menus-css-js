5885134c07d7addf1d6f1061306a37842173da5f
johnsjohns <o-ana@hotmail.com>
2020-11-02 18:24:34 -0300

    Adicionado animacao no menu2

---
+++ Menu2/js/js.js
@@ -0,0 +1,27 @@
+const principal = document.querySelector("#principal");
+const configuracao = document.querySelector("#configuracao");
+
+
+/*document.getElementById('principal').addEventListener("Click", menu(principal));
+
+configuracao.getElementById('configuracao').addEventListener("click", menu(configuracao), false);*/
+
+
+function menu(event){
+    const elementos = event.querySelectorAll('a');
+    const todos = document.querySelectorAll('a');
+    todos.forEach(elemento => elemento.classList.remove("animate-down"));
+    const todosLi = document.querySelectorAll("li");
+    todosLi.forEach(elemento => elemento.style.height = '18px');
+    altura = 18;
+    var time = 0;
+        elementos.forEach(elemento =>{
+            altura = altura + 18;
+            event.style.height = altura + "px";
+            elemento.classList.add('animate-down');
+            elemento.style.animationDelay = time + "ms";
+            time += 130;
+            
+        }
+    )    
+};
